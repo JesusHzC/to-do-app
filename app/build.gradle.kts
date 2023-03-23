f121558ff301da64a555a857e4036d6fa1135d0b
Jesus Hernandez <jesus.clemente@elektra.com.mx>
2023-03-23 17:02:59 -0600

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id ("com.android.application")
     kotlin ("android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 val versions = rootProject.extra
@@ -59,18 +61,40 @@ android {
 }
 
 dependencies {
-    implementation ("androidx.core:core-ktx:1.7.0")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation ("androidx.activity:activity-compose:1.3.1")
-    implementation ("androidx.compose.ui:ui:${versions.get("compose_ui_version")}")
-    implementation ("androidx.compose.ui:ui-tooling-preview:${versions.get("compose_ui_version")}")
-    implementation ("androidx.compose.material:material:1.2.0")
-    testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:${versions.get("compose_ui_version")}")
-    debugImplementation ("androidx.compose.ui:ui-tooling:${versions.get("compose_ui_version")}")
-    debugImplementation ("androidx.compose.ui:ui-test-manifest:${versions.get("compose_ui_version")}")
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("androidx.compose.ui:ui:${versions.get("compose_ui_version")}")
+    implementation("androidx.compose.ui:ui-tooling-preview:${versions.get("compose_ui_version")}")
+    implementation("androidx.compose.material:material:1.2.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${versions.get("compose_ui_version")}")
+    debugImplementation("androidx.compose.ui:ui-tooling:${versions.get("compose_ui_version")}")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:${versions.get("compose_ui_version")}")
 
+    //Dagger Hilt
+    implementation("com.google.dagger:hilt-android:${versions.get("dagger_hilt_version")}")
+    kapt("com.google.dagger:hilt-android-compiler:${versions.get("dagger_hilt_version")}")
 
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:${versions.get("navigation_version")}")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:${versions.get("retrofit_version")}")
+    implementation("com.squareup.retrofit2:converter-gson:${versions.get("retrofit_version")}")
+
+    //OkHttp
+    implementation("com.squareup.okhttp3:okhttp:${versions.get("okhttp_version")}")
+    implementation("com.squareup.okhttp3:logging-interceptor:${versions.get("okhttp_version")}")
+
+    //Room
+    implementation("androidx.room:room-runtime:${versions.get("room_version")}")
+    implementation("androidx.room:room-ktx:${versions.get("room_version")}")
+    kapt("androidx.room:room-compiler:${versions.get("room_version")}")
+}
+
+kapt {
+    correctErrorTypes = true
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,16 @@
 extra.apply {
     set("compose_ui_version", "1.2.0")
+    set("dagger_hilt_version", "2.44")
+    set("navigation_version", "2.5.3")
+    set("retrofit_version", "2.9.0")
+    set("okhttp_version", "4.10.0")
+    set("room_version", "2.4.3")
 }
 
 plugins {
-    id ("com.android.application") version "7.4.2" apply false
-    id ("com.android.library") version "7.4.2" apply false
-    id ("org.jetbrains.kotlin.android") version "1.7.0" apply false
+    id("com.android.application") version "7.4.2" apply false
+    id("com.android.library") version "7.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.0" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
     `kotlin-dsl`
 }
