21028e371b9ec480c2eb14c626892133029ab860
Jesus Hernandez <jesus.clemente@elektra.com.mx>
2023-03-23 19:51:01 -0600

    Setup Hilt

--- app/src/main/java/com/github/jesushzc/todoapplication/MainActivity.kt
+++ app/src/main/java/com/github/jesushzc/todoapplication/MainActivity.kt
@@ -11,7 +11,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.github.jesushzc.todoapplication.ui.theme.ToDoApplicationTheme
-
+import dagger.hilt.android.AndroidEntryPoint
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
